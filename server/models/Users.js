2f128eed9102fd8c2d924317226cc6cc8054fc9b
Victor Bonanho <victbonanho@gmail.com>
2024-07-07 16:51:23 -0300

    Login system

---
+++ server/models/Users.js
@@ -0,0 +1,19 @@
+module.exports = (squelize, DataTypes) => {
+  const Users = squelize.define("Users", {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+
+  // Users.associate = (models) => {
+  //   Users.hasMany(models.Posts, {
+  //     onDelete: "cascade",
+  //   });
+  // };
+  return Users;
+};
---
+++ server/routes/Users.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const router = express.Router();
+const { Users } = require("../models");
+const bcrypt = require("bcrypt");
+
+const { sign } = require("jsonwebtoken");
+
+router.post("/", async (req, res) => {
+  const { username, password } = req.body;
+  bcrypt.hash(password, 10).then((hash) => {
+    Users.create({
+      username: username,
+      password: hash,
+    });
+    res.json("User Registered");
+  });
+});
+
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  const user = await Users.findOne({ where: { username: username } });
+
+  if (!user) {
+    res.json({ error: "User Doesn't Exist" });
+  } else {
+    bcrypt.compare(password, user.password).then((match) => {
+      if (!match) {
+        res.json({ error: "Wrong Username And Password Combination" });
+      } else {
+        const acessToken = sign(
+          { username: user.username, id: user.id },
+          "importantsecret"
+        );
+        res.json(acessToken);
+      }
+    });
+  }
+});
+
+module.exports = router;
